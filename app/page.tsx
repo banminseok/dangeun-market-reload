7798a1c683791314ba31fe087090b88f5892e10d
banminseok <halfmin@gmail.com>
2024-09-07 11:23:33 +0900

    4.1 init

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-gray-700">
-      <div className="flex flex-col gap-3 w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl ">
-        {["Nico","Me","You","ban",""].map((person,index)=>
-        <div key={index} className="flex items-center gap-5 group">
-          <div className="size-7 bg-blue-400 rounded-sexy-name "/>
-          <span className="text-lg font-medium group-hover:text-red-500 group-focus-within:">{person}</span>
-          <div className="size-6 animate-none bg-red-500 text-white flex items-center justify-center rounded-full
-          relative">
-            <span className="z-10">{index}</span>
-            <div className="size-6 bg-red-500 rounded-full absolute animate-ping"/>
-          </div>
-        </div>
-        )}
-        <div>
-          <input type="text"/>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link href="/create-account" 
+        className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center  hover:bg-orange-400 transition-colors">시작하기</Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
