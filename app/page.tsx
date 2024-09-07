be3617b7ce2bb33b342335122c4ada9268a14bfb
banminseok <halfmin@gmail.com>
2024-09-07 09:55:20 +0900

    3.9 group, -hover, -focus-within

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-gray-700">
       <div className="flex flex-col gap-3 w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl ">
         {["Nico","Me","You","ban",""].map((person,index)=>
-        <div key={index} className="flex items-center gap-5 ">
+        <div key={index} className="flex items-center gap-5 group">
           <div className="size-7 bg-blue-400 rounded-full"/>
-          <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">{person}</span>
+          <span className="text-lg font-medium group-hover:text-red-500">{person}</span>
           <div className="size-6 animate-none bg-red-500 text-white flex items-center justify-center rounded-full
           relative">
             <span className="z-10">{index}</span>
