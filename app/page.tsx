050b98c6742205206a1f5da894ac1f9e6054d83f
banminseok <halfmin@gmail.com>
2024-09-07 10:09:26 +0900

    3.10 tailwind.config extended

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
       <div className="flex flex-col gap-3 w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl ">
         {["Nico","Me","You","ban",""].map((person,index)=>
         <div key={index} className="flex items-center gap-5 group">
-          <div className="size-7 bg-blue-400 rounded-full"/>
-          <span className="text-lg font-medium group-hover:text-red-500">{person}</span>
+          <div className="size-7 bg-blue-400 rounded-sexy-name "/>
+          <span className="text-lg font-medium group-hover:text-red-500 group-focus-within:">{person}</span>
           <div className="size-6 animate-none bg-red-500 text-white flex items-center justify-center rounded-full
           relative">
             <span className="z-10">{index}</span>
@@ -13,6 +13,7 @@ export default function Home() {
           </div>
         </div>
         )}
+
       </div>
     </main>
   );
